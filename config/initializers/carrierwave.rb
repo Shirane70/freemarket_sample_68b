1c7821a09812f12b50460a4ed3a6dacc03fdc71b
AsanoSogen <AsanoSogen>
2020-02-14 23:31:08 +0900

    Update carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,15 +3,18 @@ require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
 
 CarrierWave.configure do |config|
-  config.storage = :fog
-  config.fog_provider = 'fog/aws'
-  config.fog_credentials = {
-    provider: 'AWS',
-    aws_access_key_id: Rails.application.credentials[:aws][:access_key_id],
-    aws_secret_access_key: Rails.application.credentials[:aws][:secret_access_key],
-    region: 'ap-northeast-1'
-  }
-
-  config.fog_directory  = 'freemarket-sample-68b'
-  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/freemarket-sample-68b'
+  if Rails.env.development? || Rails.env.test? #開発とテストは今まで通りに
+    config.storage = :file
+  elsif Rails.env.production? #本番はS3に保存する
+    config.storage = :fog
+    config.fog_provider = 'fog/aws'
+    config.fog_credentials = {
+      provider: 'AWS',
+      aws_access_key_id: Rails.application.credentials.aws[:access_key_id],
+      aws_secret_access_key: Rails.application.credentials.aws[:secret_access_key],
+      region: 'ap-northeast-1'
+    }
+    config.fog_directory  = 'freemarket-sample-68b'
+    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/freemarket-sample-68b'
+  end
 end
