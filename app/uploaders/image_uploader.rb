791bf33b57021d44cb8aece3affb3c4e6fb2606a
AsanoSogen <AsanoSogen>
2020-02-17 13:51:03 +0900

    Delete image_uploader.rb

--- app/uploaders/image_uploader.rb
+++
@@ -1,9 +0,0 @@
-if Rails.env.development? || Rails.env.test?
-  storage :file
-else
-  storage :fog
-end
-
-def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
-end
