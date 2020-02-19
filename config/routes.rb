81982ef655e062db53059931c739298411ab61e4
Shirane70 <shirane.naoki.tree@ezweb.ne.jp>
2020-02-19 17:19:21 +0900

    conflict-clear

--- config/routes.rb
+++ config/routes.rb
@@ -4,15 +4,15 @@ Rails.application.routes.draw do
   root to: "informations#index" 
   resources :informations, only: [:index , :show]
 
-  root to: "users#howToPay"
+  #root to: "users#howToPay"
   # get 'users/logout' to: 'user#logout'
-  resources :informations, only: [:index]
-  resources :tests, only: [:show]
+  #resources :informations, only: [:index]
+  #resources :tests, only: [:show]
   # resources :users, only: [:index]
-  resources :users do
-    collection do
-      get :logout
-      get :howToPay
-    end
-  end 
+  #resources :users do
+    #collection do
+      #get :logout
+      #get :howToPay
+    #end
+  #end 
 end
